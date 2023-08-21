c70404e15a7a3c9c0c061aa4651e49e2ddcbbbc9
sfgserr <dead01r@gmail.com>
2023-08-21 20:07:29 +0300

    add UserDataContext

---
+++ DTORestApiExample.Data/UserDataContext.cs
@@ -0,0 +1,10 @@
+﻿using DTORestApiExample.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTORestApiExample.Data
+{
+    public class UserDataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
